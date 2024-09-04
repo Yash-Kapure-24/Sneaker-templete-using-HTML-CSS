f2b0697de0994c8d9274de9d5c35b8333ff8466e
Code-With-Yash <yashkapure080@gmail.com>
2024-09-04 11:32:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger').addEventListener('click', function () {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
